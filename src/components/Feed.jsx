d9827bd8dd935f161e27ba7e991b12c75193d575
Muzammil Momin <mominmuzammil157@gmail.com>
2025-08-01 00:19:52 +0530

    developed  feed page and user cards to display on feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,39 @@
-const Feed = () => {
-  return (
-    <div>Feed</div>
-  )
-}
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addFeed } from "../utils/feedSlice";
+import { useEffect } from "react";
+import UserCard from "./UserCard";
 
-export default Feed
+const Feed = () => {
+  const feed = useSelector((state) => state.feed);   
+  const dispatch = useDispatch();
+
+  const getFeed = async () => {
+    try {
+      if (feed) return;
+
+      const res = await axios.get(BASE_URL + "/feed", {
+        withCredentials: true,
+      });
+
+      if (res.status === 200) {
+        dispatch(addFeed(res.data));
+      }
+    } catch (err) {
+      console.error("Error fetching feed:", err);
+    }
+  };
+
+  useEffect(() => {
+    getFeed();
+  }, []);
+
+  return feed && (
+    <div className="flex items-center justify-center">
+      <UserCard user={feed[6]} />
+    </div>
+  );
+};
+
+export default Feed;
---
+++ src/components/UserCard.jsx
@@ -0,0 +1,39 @@
+const UserCard = ({ user }) => {
+  const { firstName, lastName, imageURL, bio, age, gender, skills } = user;
+  if (!user) return null;
+
+  return (
+    <div className="card bg-base-200 w-96 shadow-sm mt-7">
+      <div className="card-body items-center">
+        <div className="avatar">
+          <figure className="mask mask-heart w-34">
+            <img src={imageURL} alt={firstName} />
+          </figure>
+        </div>
+
+        <h2 className="card-title">
+          {firstName} {lastName}
+        </h2>
+        {bio && <p>{bio}</p>}
+        {age && <p>Age: {age} years</p>}
+        {gender && <p>{gender}</p>}
+        {skills.length > 0 && (
+          <ul>
+            <h3>Skills:</h3>
+
+            {skills.map((skill, index) => (
+              <li key={index}>{skill}</li>
+            ))}
+          </ul>
+        )}
+
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Interested</button>
+          <button className="btn btn-ghost">Ignore</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
